db43a2fd6d2ead3f42d5ec4396d9f518dcb1a177
Chaitanyaup70 <106642660+Chaitanyaup70@users.noreply.github.com>
2023-08-03 18:40:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function img1(){
+  var img1=document.getElementById("img1");
+  img1.style.borderRadius="20%";
+}
+function img2(){
+    var img2=document.getElementById("img2");
+    img2.style.borderRadius="20%";
+  }
+  function img3(){
+    var img3=document.getElementById("img3");
+    img3.style.borderRadius="20%";
+  }
+function img1o(){
+    var img1o=document.getElementById("img1");
+    img1o.style.borderRadius="0%"
+}  
+function img2o(){
+    var img2o=document.getElementById("img2");
+    img2o.style.borderRadius="0%"
+}  
+function img3o(){
+    var img3o=document.getElementById("img3");
+    img3o.style.borderRadius="0%"
+}  
